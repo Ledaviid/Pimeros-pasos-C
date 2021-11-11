4ca75e2670896c3254fef7c769e120a66de8b2f6
Davidtpz <78239912+Ledaviid@users.noreply.github.com>
2021-11-11 15:08:46 -0600

    Add files via upload
    Numero factorial, mediante salida explicita con prototipo.

---
+++ Numero factorial con prototipo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#define N 5
+int factorial(int n){
+	int i, fact=1;
+	for(i=1;i<=N;i++){
+			fact=fact*i;
+}
+}
+int main(){
+	int i, fact=1;
+	int res=0;
+  factorial(N);
+	printf("EL factorial de N es: %d",N,fact);
+ return 0;
+}
+

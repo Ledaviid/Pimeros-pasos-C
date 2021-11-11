8d46e067ef869de5244ae027bb338c5a0907a916
Davidtpz <78239912+Ledaviid@users.noreply.github.com>
2021-11-10 21:27:50 -0600

    Add files via upload
    Codicion do-while repite la condicion si es que hay un numero negativo o es mayor que 100,lleva un bucle for para sumar los numero ingresados.

---
+++ Condicion do while.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	int edad,n,suma=0,i=0,promedio;
+	
+printf("ingrese el numero de personas a validar su edad");
+scanf("%d",&n);
+
+for(i=1;i<=n;i++){
+do{
+printf("Ingrese la edad numero %d",i);
+scanf("%d",&edad);
+
+}while(edad>100 || edad<0);
+suma+=edad;
+}
+
+promedio=suma/n;
+printf("La edad promedio es:%d",promedio);
+
+	return 0;
+}
+

fe1301cc0e76098bfa36f6f4bcaa66316ea87066
Davidtpz <78239912+Ledaviid@users.noreply.github.com>
2021-11-11 15:42:50 -0600

    Add files via upload
    En este programa sencillo se utiliza un while pra que el usuario ingrese 5 valores  pares y si no se cumple se retorna hasta que sean los 5 numeros pares.

---
+++ Ingreso de numeros pares hasta que se cumpla que sean pares.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+int num,contador=0;
+	printf("ingrese 5 numeros pares:\n");
+	
+while(contador < 5){
+	scanf("%d",&num);
+	if(num %2 ==0)
+		contador++;
+	
+}
+	return 0;
+
+}
